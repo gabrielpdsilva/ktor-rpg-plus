5baa52cc9f6f348c00403784d1e53b469f9fabc9
Gabrielpdsilva <gabriel.pimentel.sotc@gmail.com>
2022-11-12 14:49:04 -0300

    Organizing code

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
-val exposedVersion: String by project
+val exposed_version: String by project
+val ktorm_version: String by project
+val mssql_jdbc_version: String by project
 
 plugins {
     application
@@ -22,7 +24,7 @@ application {
 repositories {
     mavenCentral()
 }
-//implementation("com.microsoft.sqlserver:mssql-jdbc:6.1.0.jre7")
+
 dependencies {
     implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
@@ -31,14 +33,12 @@ dependencies {
     implementation("io.ktor:ktor-server-host-common-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
-    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
-    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
-    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
 
-
-
-    implementation("org.ktorm:ktorm-core:3.5.0")
-    implementation("com.microsoft.sqlserver:mssql-jdbc:11.2.1.jre11")
+    implementation("org.ktorm:ktorm-core:$ktorm_version")
+    implementation("com.microsoft.sqlserver:mssql-jdbc:$mssql_jdbc_version")
 
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
